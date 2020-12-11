41835103410fae279ecb91766a3ec4b25e1e3d29
ahmadfaqih-dev <faqih.elekyoben@gmail.com>
2020-12-11 09:50:27 +0700

    membuat styling page create blog

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,5 @@
 import RegisterBg from './img/register-bg.jpg';
-import LoginBg from './img/register-bg.jpg';
+import LoginBg from './img/login.jpg';
 import Post from './img/post.jpg';
 
 export {RegisterBg, LoginBg, Post};
--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Gap, TextArea, Upload } from '../../components/atoms'
+import Button from '../../components/atoms/Button'
+import Input from '../../components/atoms/Input'
+import './createBlog.scss'
 
 const CreateBlog = () => {
     return (
-        <div>
-            <p>Isi content blog</p>
+        <div className="blog-post">
+            <p className="title">Create New Blog Post</p>
+            <Input label="Post Titile"/>
+            <Upload />
+            <TextArea />
+            <Gap height={20} />
+            <div className="button-action">
+            <Button title="Save"/>
+            </div>
         </div>
     )
 }
