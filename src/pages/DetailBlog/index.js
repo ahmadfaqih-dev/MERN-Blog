b1acf484df73d0d8149827ee561dcc8f00c78b4c
ahmadfaqih-dev <faqih.elekyoben@gmail.com>
2020-12-13 13:04:40 +0700

    membuat styling detail blog

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {Post} from '../../assets';
+import './detailBlog.scss';
 
 const DetailBlog = () => {
     return (
-        <div>
-            <p>Isi Detail Blog</p>
+        <div className="setail-blog-wrapper">
+            <img className="img-cover" src={Post} alt="thumb"/>
+            <p className="blog-title">Title Blog</p>
+            <p className="blog-author">Author - Date Post</p>
+            <p className="blog-body">Content BLog</p>
         </div>
     )
 }
